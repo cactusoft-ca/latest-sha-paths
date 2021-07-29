0ed91bfaa8d351c32a41354a6d11cb0c0256418e
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2021-07-29 11:33:10 -0700

    Printing the sha of each path

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
 import {wait} from './wait'
 
 async function run(): Promise<void> {
@@ -6,10 +8,35 @@ async function run(): Promise<void> {
 
     const paths = core.getMultilineInput('paths', { required: true });
 
+
+    // printing the list of paths provided
+    core.debug('List of provided paths:')
     for (let path of paths) {
       core.debug(path)
     }
 
+    let myOutput = '';
+    let myError = '';
+
+    // printing the sha of each path
+    for (let path of paths) {
+      const options = {
+        listeners: {
+          stdout: (data: Buffer) => {
+            myOutput += data.toString();
+          },
+          stderr: (data: Buffer) => {
+            myError += data.toString();
+          }
+        }
+      };
+
+      await exec.exec('git', ['log', '--pretty=format:"%H"', '-n1', path], options);
+      core.debug(myOutput)
+      core.info(myOutput)
+      console.log(myOutput);
+      console.log(myError);
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
